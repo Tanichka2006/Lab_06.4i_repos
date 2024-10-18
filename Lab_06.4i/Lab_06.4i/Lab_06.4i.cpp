335d5ae24c60690e6eac41f40eae72c74a7db2ab
Tanichka2006 <tanya4ych@gmail.com>
2024-10-18 21:09:51 +0300

    Add files via upload

---
+++ Lab_06.4i/Lab_06.4i/Lab_06.4i.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+
+
+void CreateArray(double* arr, const int size, const int Low, const int High) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = Low + rand() % (High - Low + 1) + (rand() % 100) / 100.0;  
+    }
+}
+
+
+int CountLessThanC(const double* arr, const int size, const double C) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < C) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int SumAfterLastNegative(const double* arr, const int size) {
+    int sum = 0;
+    int lastNegativeIndex = -1;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            lastNegativeIndex = i;
+        }
+    }
+    if (lastNegativeIndex != -1) {
+        for (int i = lastNegativeIndex + 1; i < size; i++) {
+            sum += (int)arr[i]; 
+        }
+    }
+    return sum;
+}
+
+
+void TransformArray(double* arr, const int size) {
+    double maxElement = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    double* temp = new double[size];
+    int j = 0;
+
+   
+    for (int i = 0; i < size; i++) {
+        if (fabs(arr[i] - maxElement) / maxElement <= 0.2) {
+            temp[j++] = arr[i];
+        }
+    }
+
+   
+    for (int i = 0; i < size; i++) {
+        if (fabs(arr[i] - maxElement) / maxElement > 0.2) {
+            temp[j++] = arr[i];
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = temp[i];
+    }
+
+    delete[] temp;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int n;
+    double C;
+    cout << "Enter the size of the array (n): ";
+    cin >> n;
+    cout << "Enter the value of C: ";
+    cin >> C;
+
+    if (n <= 0) {
+        cout << "Size must be positive." << endl;
+        return 1;
+    }
+
+    double* arr = new double[n];
+    int Low = -10;
+    int High = 10;
+
+    CreateArray(arr, n, Low, High);
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // 1.1. Кількість елементів менших за C
+    int count = CountLessThanC(arr, n, C);
+    cout << "Count of elements less than C: " << count << endl;
+
+    // 1.2. Сума цілих частин після останнього від'ємного
+    int sum = SumAfterLastNegative(arr, n);
+    cout << "Sum of integer parts after last negative element: " << sum << endl;
+
+    // 2. Перетворення масиву
+    TransformArray(arr, n);
+
+    cout << "Transformed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+
+    return 0;
+}
---
+++ Lab_06.4i/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06.4i/Lab_06.4i.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int size = 10;
+			const double arr[size] = { 34,54,68,-24,57,45,67,34,-57,34 };
+			int sum = SumAfterLastNegative(arr, size);
+			Assert::AreEqual(sum, 34);
+			
+		}
+	};
+}
